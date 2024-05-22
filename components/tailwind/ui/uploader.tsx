004717e7ea4c3de326036f7b6271e62688932cf1
Vaibhav Ojha <Vaibhav.Ojha@in.ey.com>
2024-05-23 03:15:07 +0530

    Adding dropzone in UI for file upload

--- components/tailwind/ui/uploader.tsx
+++ components/tailwind/ui/uploader.tsx
@@ -1,14 +1,17 @@
+'use client';
 import React from 'react';
 import { UploadIcon } from "lucide-react";
+import { Dropzone } from './Dropzone';
 
 const Uploader
  = () => {
   return (
-    <button className="rounded-lg flex w-full justify-center items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8">
-      <UploadIcon className="mx-2
-" />
-      Upload Report
-    </button>
+    <section className='section'>
+      <div className='container'>
+        <h1 className='title text-3xl font-bold'>Upload Files</h1>
+        <Dropzone className='p-16 mt-10 border border-neutral-200' />
+      </div>
+    </section>
   );
 };
 
