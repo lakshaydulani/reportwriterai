032c967730826bc94fb09a2ad24a1ecc91472ccc
vaibha.k.ojha <vaibha.k.ojha@gmail.com>
2024-06-03 11:44:47 +0000

    logo in Animation

--- components/tailwind/ui/animation.tsx
+++ components/tailwind/ui/animation.tsx
@@ -1,39 +1,29 @@
-import { motion, AnimatePresence } from "framer-motion"
-import { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from "framer-motion";
+import Image from "next/image";
+import { useEffect } from 'react';
 
 const Animation = ({ setWelcomeScreenVisible }) => {
-    useEffect(() => {
-      const timer = setTimeout(() => {
-        setWelcomeScreenVisible(false);
-      }, 3000); // Duration for which the welcome screen is visible
-  
-      return () => clearTimeout(timer);
-    }, [setWelcomeScreenVisible]);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setWelcomeScreenVisible(false);
+    }, 3000); // Duration for which the welcome screen is visible
+
+    return () => clearTimeout(timer);
+  }, [setWelcomeScreenVisible]);
+
   return (
     <AnimatePresence>
-        <motion.div
+      <motion.div
         initial={{ opacity: 1, y: 0 }}
         animate={{ opacity: 0, y: -100 }}
         transition={{ duration: 1, delay: 2.5 }}
-        style={{
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            backgroundColor: 'black',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            color: 'white',
-            fontSize: '2rem',
-            zIndex: 1000
-        }}
-        >
-            Welcome To EY Report Writting
-        </motion.div>
+        className="fixed top-0 left-0 w-full h-full animation flex flex-col items-center justify-center text-white text-2xl z-50"
+      >
+        <Image src="/images/eylogo.png" alt="Logo" width={100} height={100} />
+        <p className="mt-4">Welcome To EY Report Writer</p>
+      </motion.div>
     </AnimatePresence>
-  )
+  );
 }
 
-export default Animation
+export default Animation;
