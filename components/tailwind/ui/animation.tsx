901166dba59a021408264304f64587cc35ce3efa
Lakshay Dulani <lakshaydulani@gmail.com>
2024-06-04 08:39:30 +0000

    loader changes

--- components/tailwind/ui/animation.tsx
+++ components/tailwind/ui/animation.tsx
@@ -1,27 +1,22 @@
 import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
-import { useEffect } from 'react';
+import CrazySpinner from "@/components/tailwind/ui/icons/crazy-spinner";
 
 const Animation = () => {
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setWelcomeScreenVisible(false);
-  //   }, 3000); // Duration for which the welcome screen is visible
-
-  //   return () => clearTimeout(timer);
-  // }, [setWelcomeScreenVisible]);
 
   return (
     <AnimatePresence>
       <motion.div
         initial={{ y: 0 }}
         animate={{ y: "-100vh" }}
-        transition={{ duration: 0.75, delay: 2 }}
-        className="fixed top-0 left-0 w-full h-full animation flex flex-col items-center justify-center text-white text-2xl z-50"
+        transition={{ duration: 0.75, delay: 20 }}
+        className="fixed top-0 left-0 w-full h-full animation flex flex-col items-center justify-center text-white z-50"
       >
         <Image src="/images/eylogo.png" alt="Logo" width={100} height={100} />
-        <p className="mt-3 text-3xl">AI Report Writer</p>
-        <p className="mt-3 text-sm">Preparing the editor..</p>
+        <p className="mt-5 text-3xl">AI Report Writer</p>
+        <p className="mt-3 text-sm">Preparing the editor</p>
+        <p className="mt-5"><CrazySpinner color="white" /></p>
+
       </motion.div>
     </AnimatePresence>
   );
