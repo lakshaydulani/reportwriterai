2276f9dd9fcedcc25293510e4fa1e823a4621222
vaibha.k.ojha <vaibha.k.ojha@gmail.com>
2024-07-09 09:05:03 +0000

    Langchain API Integration

--- app/api/langchain/route.tsx
+++ app/api/langchain/route.tsx
@@ -1,27 +1,30 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
 
-export async function POST(request: Request) {
+export async function POST(request) {
     try {
         const content = await request.json(); // Directly get the content array
         console.log('Request content is:', content);
 
-        const apiURL = 'https://ai-report-writer.azurewebsites.net/api/download_file';
+        const apiURL = 'https://langchaintest.azurewebsites.net/api/azureTest';
 
         const apiResponse = await fetch(apiURL, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(content),  // Send content array to external API
-        });
+            body: JSON.stringify(content),  // Convert content to JSON string
+        }); 
+        console.log(apiResponse.status);
 
-        if (!apiResponse.ok) {
+        if (apiResponse.status !== 200) {
             const errorText = await apiResponse.text();
             console.error("Error response from external API:", errorText);
-            return new NextResponse("You encountered an error: " + errorText, { status: apiResponse.status });
+            return new NextResponse(`You encountered an error: ${errorText}`, { status: apiResponse.status });
         }
 
-        const data = await apiResponse.json();
+        const data = await apiResponse.text();
+        console.log("data from API route is :\n", data);
+
         return new NextResponse(JSON.stringify(data), {
             status: 200,
             headers: { 'Content-Type': 'application/json' }
