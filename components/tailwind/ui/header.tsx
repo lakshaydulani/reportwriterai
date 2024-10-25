5c0e3b2243c5f94e321aba304c1cf0de41c8419b
vaibha.k.ojha <vaibha.k.ojha@gmail.com>
2024-10-25 07:20:27 +0000

    UI changes as per figma

--- components/tailwind/ui/header.tsx
+++ components/tailwind/ui/header.tsx
@@ -4,22 +4,16 @@ import { HEADER_TITLE } from "@/lib/constants";
 
 const Header = () => {
   return (
-    <header className="px-4 py-2 flex items-center ">
+    <header className="bg-header-grey px-4 py-2 flex items-center ">
       <div className="flex items-center">
       <Image
-          src="/images/logoey.png"
+          src="/images/eylogo.png"
           alt="Logo"
           width={50}
           height={50}
           // className="mr-4"
         />
-        <h1 className="text-4xl font-bold mt-1 italic ms-1">{HEADER_TITLE}</h1>
-        <Image
-          src="/images/aiicon.png"
-          alt="AILogo"
-          width={20}
-          height={20}
-        />
+        <h1 className="text-2xl text-white font-bold mt-1 ms-1">{HEADER_TITLE}</h1>
       </div>
       <div className="float-end ml-auto pt-2" title="How To Use">
         <MessageCircleIcon />
